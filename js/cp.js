dfcfb40e99f3b6a46c8aa0a5ac6c97809ead926c
tgaw <tgaw@408e401b-a8c4-4a28-8c6d-64d0353487e0>
2010-09-22 05:17:49 +0000

    Building hashchange system

--- js/cp.js
+++ js/cp.js
@@ -9,7 +9,7 @@
 var CP = function ()
 {	
 	var $this    = this,
-		sets     = {},
+		sets     = [],
 		photos   = {},
 		internal = {};
 	
@@ -35,8 +35,119 @@ var CP = function ()
 				}
 			);
 			
-			$('header:eq(0) ul').prepend(html.join(''));
+			$('header:eq(0) ul').prepend(html.join('')).show();
+			
+			internal.address.init();
 		}
+	};
+	
+	// Using jquery.address, handle hashchanges
+	internal.address = {
+		
+		init: function ()
+		{
+			var changeWasCalled = false;
+			
+			// Bind the address change event to our load page
+			$.address.change(
+				function (e)
+				{			
+					changeWasCalled = true;
+					internal.loadSection(e);
+				}
+			);
+			
+			// For all a tags, route their click even to the address event
+			// NOTE: We look for the rel attribute instead of the href as
+			// a workaround for IE. For all IE <a>s the href automatically
+			// has the full URL prepended to it. We don't want that.
+			$('a').address(
+				function () 
+				{
+					if ($(this).attr('rel'))
+					{
+						return $(this).attr('rel');
+					}
+				}
+			);
+			
+			// When initialy loaded, or loaded without a specific URL 
+			// we need to direct it to the correct page
+			/*
+			if ($.address.value() === '/')
+			{
+				$.address.value('/foobar');
+			}
+			*/
+
+			// We were having trouble with the address change() event not firing
+			// when refreshing the page, or navigating straight to a hashed page
+			// so we need to check to see if change has not been called and act.
+			if (!changeWasCalled)
+			{
+				$.address.update();
+			}
+		}
+	};
+	
+	internal.loadSection = function (sectionInfo)
+	{
+		var sectionName = sectionInfo.pathNames[0];
+		console.log(photos[sectionName][0]);
+	};
+	
+	// Take an array of photo object, sort them by their set name
+	// then create multiple objects grouped by the name
+	// Object created example:
+	// photos['childhood']
+	internal.organizePhotos = function (photos)
+	{
+		var sorted = photos.sort(function (a, b)
+			{
+				var A = a.set.toLowerCase(),
+					B = b.set.toLowerCase(),
+					sort = 0;
+				
+				if (A < B)
+				{
+					sort = -1;
+				}
+				else if (A > B)
+				{
+					sort = 1;
+				}
+				else
+				{
+					sort = 1;
+				}
+				
+				return sort;
+			}),
+			
+			prevSet = null,
+			
+			org = {};
+			
+		$.each(sorted, 
+			function (i, p)
+			{
+				if (p.set !== prevSet)
+				{
+					if (prevSet !== null)
+					{
+						org[prevSet].sort();
+					}
+					
+					org[p.set] = [];
+				}
+				
+				org[p.set].push(p.url);
+				
+				prevSet = p.set;
+			}
+		);
+		
+		return org;
 	};
 	
 	// Base URL for all API data
@@ -60,13 +171,12 @@ var CP = function ()
 		this.loadData(this.url('sets'),
 			function (data)
 			{
-				
 				sets = data.sets;
 				
 				$this.loadData($this.url('photos'),
 					function (data)
 					{
-						photos = data.photos;
+						photos = internal.organizePhotos(data.photos);
 						
 						$this.allDataLoaded();
 					}
